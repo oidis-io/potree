f5dc5f4fc9bb6fee623963a3f93dc9a89dec2af4
Michal Kelnar <kelnar.michal@gmail.com>
2025-10-08 08:38:31 +0200

    upgraded eslint config (rules WIP)

---
+++ eslint.config.mjs
@@ -0,0 +1,68 @@
+/*! ******************************************************************************************************** *
+ *
+ * Copyright 2025 Oidis
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ * The BSD-2-Clause license for this file can be found in the LICENSE.txt file included with this distribution
+ * or at https://spdx.org/licenses/BSD-2-Clause.html#licenseText
+ *
+ * ********************************************************************************************************* */
+
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import unusedImports from "eslint-plugin-unused-imports";
+
+export default [
+    {
+        ignores: [".github/", ".idea/", "build/", "node_modules/", "resources/", "pointclouds/", "docs/", "examples/", "libs/", "**/*.min.js", "gulpfile.js", "rollup.*.js"],
+    },
+    {
+        languageOptions: {
+            globals: globals.browser
+        }
+    },
+    pluginJs.configs.recommended,
+    {
+        plugins: {
+            "unused-imports": unusedImports
+        },
+        languageOptions: {
+            globals: {
+                "$": "readonly",
+            }
+        },
+        rules: {
+            semi: "error",
+            "no-unused-imports": "off",
+            "no-unused-vars": "off",
+            "unused-imports/no-unused-imports": "error",
+            "unused-imports/no-unused-vars": "off",
+            "no-undef": "off",
+            "no-empty": "off",
+            "no-prototype-builtins": "off",
+            "no-case-declarations": "off",
+            "no-debugger": "off",
+            "no-dupe-class-members": "off",
+            "no-constant-binary-expression": "off",
+            "getter-return": "off",
+            "no-global-assign": "off",
+            "no-redeclare": "off"
+            // "unused-imports/no-unused-vars": [
+            //     "warn",
+            //     {
+            //         "vars": "all",
+            //         "varsIgnorePattern": "^_",
+            //         "args": "after-used",
+            //         "argsIgnorePattern": "^_",
+            //     }
+            // ],
+            // "sort-imports": ["error", {
+            //     "ignoreCase": false,
+            //     "ignoreDeclarationSort": false,
+            //     "ignoreMemberSort": false,
+            //     "memberSyntaxSortOrder": ["none", "all", "multiple", "single"],
+            //     "allowSeparatedGroups": false
+            // }]
+        }
+    }
+];
