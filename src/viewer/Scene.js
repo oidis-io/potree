f2271d72d200d087cf2fa2ed194edb3b951bf9ef
Michal Kelnar <kelnar.michal@gmail.com>
2025-10-31 14:14:53 +0100

    temporary suppress camera selection for FLAT (workaround for bug - pan with ortho cam)

--- src/viewer/Scene.js
+++ src/viewer/Scene.js
@@ -15,7 +15,6 @@ import { CameraMode } from "../defines.js";
 import { View } from "./View.js";
 import { Utils } from "../utils.js";
 import { EventDispatcher } from "../EventDispatcher.js";
-import PotreeConfig, { ViewMode } from "../PotreeConfig.js";
 
 export class Scene extends EventDispatcher {
     constructor() {
@@ -350,17 +349,19 @@ export class Scene extends EventDispatcher {
             return this.overrideCamera;
         }
 
-        if (PotreeConfig.viewMode === ViewMode.FLAT) {
+        // TODO(mkelnar) render re/positioning with cesium not working properly for ortho camera (looks like problem with pivot based calc).
+        //  Necessary to validate root cause and also try to figure out how to use "pivot" as true origin of scene.
+        // if (PotreeConfig.viewMode === ViewMode.FLAT) {
+        //     return this.cameraO;
+        // } else {
+        if (this.cameraMode === CameraMode.PERSPECTIVE) {
+            return this.cameraP;
+        } else if (this.cameraMode === CameraMode.ORTHOGRAPHIC) {
             return this.cameraO;
-        } else {
-            if (this.cameraMode === CameraMode.PERSPECTIVE) {
-                return this.cameraP;
-            } else if (this.cameraMode === CameraMode.ORTHOGRAPHIC) {
-                return this.cameraO;
-            } else if (this.cameraMode === CameraMode.VR) {
-                return this.cameraVR;
-            }
+        } else if (this.cameraMode === CameraMode.VR) {
+            return this.cameraVR;
         }
+        // }
 
         return null;
     }
